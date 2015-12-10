8d9930af9286507db3248cfb81635f7a75883b56
marco <marco.servetto@gmail.com>
2015-12-10 14:02:31 +0100

    Updates-no generation

--- GettingStarted/index.c
+++ GettingStarted/index.c
@@ -1,14 +1,29 @@
 #include "header.h"
+WTitle(Why 42)
+42 is a new programming language,
+the name is inspired by The Hitchhiker's Guide to the Galaxy.
+The language goal is to allow the transparent cooperation and composition of millions of libraries at the same time.
+In 42 you will be able to import libraries just by naming them and to
+compose them in your code without worrying of their dependencies.
+Normally libraries are big things, coming from outside and manually imported/installed. In 42 Libraries are first class entities, that can be manipulated as values,loaded, stored, modified, adapted, saved or simply used. You will write code that manipulate libraries, but more importantly you will use libraries that manipulate libraries in non trivial ways.
+
+WP
+42 enforce a high level of abstraction, thus there is no access to primitive
+structures like arrays (just use collections), and there is no concept of null or default initialization.
+While is possible to write in a fully functional style in 42, usually 42 programs mix mutable datastructure with aliasing and immutable data as found in functional languages. Indeed 42 offers powerful support for aliasing control and immutability, helping in mixing the two stiles in an easy and natural way.
+
+WP
+42 offers (well, will offer) automatic parallelism: code that can safely be executed in parallel may be automatically optimized.
+
 WTitle(Object orientation)
 42 is a pure object oriented language,
-where every value is an object.
-If you come from other languages like Java this means that
+where every value is an object. This means that:
 <ul>
 <li>
 Numbers are objects, 
 so thay can have methods like Wcode(.sqrt()) or Wcode(.abs()). 
 </li><li>
-All operators are just method calls, thus Wcode(a+b) is sugar for Wcode(a.operator+(b)), and so on. Note how Wcode(operator+) is a valid method name.
+All operators are just method calls, thus Wcode(a + b) is sugar for Wcode(a.operator+(b)), and so on. Note how Wcode(operator+) is a valid method name.
 Any class that offers that method supports the operator.
 
 </li><li>
@@ -16,7 +31,7 @@ Classes are objects, when you write Wcode(Foo.bar()) you refer to the method Wco
 class objects are just objects, and you can also store them in local bindings if you wish, as in Wcode(x=Foo)
 </li><li>
 The code is an object, but only at the granularity of 
-Libraries, that is, balanced pairs of curly brakets representing classes (or interfaces) with methods and nesteds.
+Libraries, that is, balanced pairs of curly brackets representing classes (or interfaces) with methods and nesteds.
 This is usefull for meta-programming, as we will see later this is the most important feature of 42.
 <!--OCode Library myCode={ method Int foo()} CCode
 A local binding of type Library initialized with a class with a single Wcode(foo()) method. Since there is no body, it is an abstract method.
@@ -24,16 +39,18 @@ A local binding of type Library initialized with a class with a single Wcode(foo
 
 </li><li>
 Differently from other pure object oriented languages, in 42
-all objects have the same treatement, both syntactically and semantically.
-For example Wcode(x=S"Hello "++S"World")
-This is a declaraton for a local binding Wcode(x), using the string class Wcode(S) and the concatenation operator Wcode(++).
-Coming from another programming language, you many be suprised that we have to write Wcode(S) before Wcode("Hello ") and Wcode("World").
-This extra verbosity is need to provide fair treatement to all classes. (Wlink(note1,see more))
-In 42 we do not give preferencial treatement
+all objects have the same treatment, both syntactically and semantically.
+For example Wcode(x = S"Hello " ++ S"World")
+This is a declaration for a local binding Wcode(x), using the string class Wcode(S) and the operator Wcode(++),
+used as string (or in general sequence/collection) concatenation.
+
+Coming from another programming language, you many be surprised that we have to write Wcode(S) before Wcode("Hello ") and Wcode("World").
+This extra verbosity is need to provide fair treatment to all classes. (Wlink(note1,see more))
+In 42 we do not give preferential treatment
 to special object/classes. In this sense, we consider
 most other languages to be racist.
 They give priority to their "preferred" version of numbers and strings, and this puts into a position of unfair disadvantage library code trying to define its own numbers kinds.
-Insead in 42 you may encounter strings like Wcode(URL"www.google.com"),
+Instead in 42 you may encounter strings like Wcode(URL"www.google.com"),
  Wcode(EMAIL"Artur.Dent@gmail.com") or multiline strings like
  OCode Javascript"
 'x=function(){
@@ -49,9 +66,9 @@ Note that we write the class name after the number, both for better readability 
 You may encounter Wcode(Int), Wcode(UInt), Wcode(Float), Wcode(Meter), Wcode(Kg), Wcode(Second), Wcode(Year), Wcode(PhoneNumber) and many other numeric classes.
 </li></ul>
 
-WTitle(Simpler complete program)
+<!--WTitle(Simpler complete program)
 
-Let now starts showing the simplest 42 progam: an empty library.
+Let now starts showing the simplest 42 program: an empty library.
 
 OCode {} CCode
 
@@ -60,6 +77,8 @@ WP
 As you see 42 is very intuitive, as you would expect from your former life experiences, most simple things just does not work.
 Note how valid programs can produce errors.
 We will soon learn how to produce errors in controlled and elegant ways.
+-->
+
 WTitle(Simple hello world program)
 Let show now a simple hello world program:
 OCode 
@@ -70,15 +89,6 @@ Main:{
   }
 }
 CCode
-WMS(cosa pensiamo di questa alternativa?)
-OCode
-{L42.is/AdamTowel
-Main:{
-  Debug.log(S"Hello world")
-  return {}
-  }
-}
-CCode
 At the right of Wcode(Main:) we write the expression/statements that
 we wish to execute. Wcode(Main) is not a method, and Wcode(Main) is not special name either. In 42 there is no concept of main method as in
 Java or C. For now you can think of Wcode(Main:) as a top level command, we will understand later how this fits with the general language design.
@@ -86,12 +96,16 @@ Java or C. For now you can think of Wcode(Main:) as a top level command, we will
 , code is simply executed from top to bottom as in 
 Python, Javascript or Php. However, the
 top level expression is a Library, and code can go in libraries 
-as an initializer foyou need to put the code into an expression
+as an initializer for you need to put the code into an expression
 -->
-Here AdamTowel is our Towel, that is the set of classes and interfaces that we wish to start from.
+When we write Wcode(reuse L42.is/AdamTowel) we are asking 42 to
+reuse the code offered by the library offered in the internet address 
+Wcode(L42.is/AdamTowel).
+AdamTowel is our Towel, that is the set of classes and interfaces that we wish to start from (Wlink(towel,see more on towels)).
+Wcode(L42.is) is the main website of 42, where most commonly used libraries are hosted. To reuse code you need internet connection; but this also means that you will never have to manually import any code. Required code will be transitivelly downloaded and cached on your machine, you need not to be aware of the existence of this mechanism.
 WP
 We are not supposed to always start from AdamTowel, there are many interesting towels out there, and you may also become skilled in the 
-advance technique of towel embrowdery.
+advance technique of towel embroidery.
 WP
 Lets now declare a method and call it.
 OCode
@@ -109,19 +123,23 @@ Main:{
 }
 CCode
 Here we declare a class to host our Wcode(hello(name)) method.
-We write Wcode(class method) to declare a method that can be called on the class object, as in Wcode(MyCode.hello(name=S"world")).
-The ++ is used as string (or in general sequence/collection) concatenation.
-
+We write Wcode(class method) to declare a method that can be called on the class object, as in Wcode(MyCode.hello(name=S"world")). This is equivalent to a static method in languages like Java or C++.
+WP
+Per Andre: MyCode.hello usa la classe, MyCode e' il receiver, MyCode e' un oggetto,
+e se vuoi puoi anche salvarlo su un binding locale, tipo x=MyCode  x.hello(...)
+WP
 Note how the method is called using the parameter name explicitly.
-We believe this increase readibilty.
+We believe this increase readability.
 WP
-You can also notice how there are two different usages for curly brakets: if there is at least a return keyword it is a set of statements,
-otherwise is a library literal, and can contains methods and other nested.
-A nested is denoted by an uppercase name, and can be obtained by a library literal or an expression producing a library literal.
-A library can be a class (default case) or an interface (starts with the Wcode(interface) keyword)
+You can also notice how there are two different usages for curly brackets: if there is at least a return keyword it is a set of statements,
+otherwise is a library literal, and can contains methods and nesteds.
+A nested (class or interface) is denoted by an upper-case name, and can be obtained by a library literal or an expression producing a library literal.
+A library can be a class (default case) or an interface (starts with the Wcode(interface) keyword).
+A nested in 42 is similar to a static inner class in Java, or a nested class in C++. It is just a convenient way to divide in a tree shape the various components of our program.
 WP
-The class Wcode(MyCode) of before offers a single class method, similar to static methods in many languages.
-You can not create instances of Wcode(MyCode), since no factory is present; you can think about it as a module.
+The class Wcode(MyCode) of before offers a single class method, have no fields and you can not create instances of Wcode(MyCode), since no factory is present; you can think about it as a module.
+In 42 we do not have constructors. Objects are created by factory methods. We believe this is a much simpler and consistent approach to object initialization.
+
 Let create now a class with state and factory:
 OCode
 Point:Data<<{
@@ -139,15 +157,15 @@ Wcode(Data) is a decorator. Decorators are classes/objects that offers an operat
 whose goal is to translate a library into a "better" library.
 In this case, Wcode(Data) is translating the class Wcode({Int x COMMA Int y}) into a much longer class, with
 a factory method taking in input the fields and initializing them, but also containing
- boring but usefull definitions for
+ boring but useful definitions for
 equality, inequality, conversions from and into human readable strings, xml and binary representations for (de)serialization.
 WP
 Finally, we declare methods to add to a single coordinate.
-For very short methods we can omit the curly brakets and return.
-Indeed method bodies are just expressions, and the curly brakets turn statements into expressions. 
+For very short methods we can omit the curly brackets and return.
+Indeed method bodies are just expressions, and the curly brackets turn statements into expressions. 
 
 In the method Wcode(add(x)) we show a constructor call and getters.
-In the method Wcode(add(y)) we show an improved version, using the with method, another gift of Data, that allows us to easly create a clone with a
+In the method Wcode(add(y)) we show an improved version, using the with method, another gift of Data, that allows us to easily create a clone with a
 single field updated.
 We can declare two methods, Wcode(add(x)) and Wcode(add(y)) with the same name, if parameter names are different.
 
@@ -159,7 +177,7 @@ Wlink(Decorators, More on decorators)
 
 WTitle(Kinds of classes)
 Wcode(Point) is a WEmph(Immutable class): none of its field can be updated or mutated.
-Immutable classes are very easy to use but may be inadeguate when representing real objects, whose state can mutate across time.
+Immutable classes are very easy to use but may be inadequate when representing real objects, whose state can mutate across time.
 A WEmph(Mutable class) is a class where the state of its instances may be mutated.
 
 Lets now declare a Wcode(Animal) mutable class, whose location can be updated.
@@ -173,7 +191,7 @@ Animal:Data<<{
 CCode
 As you see, we are using the add method from before.
 You can also notice that we are using a setter here, and that we are providing the first parameter without the argument name.
-This is accepted when writing down such name would not improve the readability, and in those cases the first parameter is convetionally called Wcode(that).
+This is accepted when writing down such name would not improve the readability, and in those cases the first parameter is conventionally called Wcode(that).
 More important, there are two new keyword:
 <ul><li>
 the location field is declared var.
@@ -235,22 +253,22 @@ OCode
   dog1.move()
   dog2.move()
 CCode
-Where the ps local variable is declared capsule, thus it can satisfy the Animal.path requirement, but it can be used only onece.
+Where the Wcode(ps) local binding is declared capsule, thus it can satisfy the Animal.path requirement, but it can be used only onece.
 dog2 have to use another capsule. It is ok to just write the object creation in place as is done.
 
 WTitle(Modifiers COMMA recall)
 We have now seen many different modifiers, lets recall them:
-immutable: the default, when you omit the modifer, you mean immutable. 
+immutable: the default, when you omit the modifier, you mean immutable. 
 An immutable reference points to an object that is never changing. Its whole reachable object graph never change and is immutable as well.
 mutable: A mutable reference...
-capsule: capsule references are used only onece and they guaranteed that the whole reachable object graph is reachable only throught that
+capsule: capsule references are used only once and they guaranteed that the whole reachable object graph is reachable only thought that
 capsule reference.
 Fields can be annotate capsule, the meaning is that they need to be initialized/updated with capsule variables. 
 class: class references denote the class object,
   on methods the meaning is the same of static methods in many languages, but it can consistently be used on parameters/local variables/fields
 to encode behaviours similar to dependency injection.
 We still need to see lent and read modifiers:
-lent: a higienic mutable reference (more on lent later).
+lent: a hygienic mutable reference (more on lent later).
 read: common supertype between immutable and mutable (and lent).
 Use read when you do not care what is the mutability of an object.
 For example, we could add to Animal
@@ -261,7 +279,7 @@ Bool isArrived()
 CCode
 This method can be called to mutable and immutable animals.
 Wait.. how can we get an immutable Animal? just make the type of the local binding explicit. The type system will take care of the rest.
-If a reference can not be safelly promoted to immutable/capsule, you may have to use the .clone() method.
+If a reference can not be safely promoted to immutable/capsule, you may have to use the .clone() method.
 OCode
   mut Animal dog1=Animal(__)
   Animal dog2=Animal(__)
@@ -273,11 +291,11 @@ CCode
 
 WTitle(Object creation COMMA recall)
 
-We have also seen many different sintactic forms to create objects: lets recall them:
+We have also seen many different syntactic forms to create objects: lets recall them:
 12Int: from a numeric representation
 S"foo": from a string representation
 Point(x=_,y=_): from the parameter values
-Points[_;_;_]: from a variable lenght sequence of values.
+Points[_;_;_]: from a variable length sequence of values.
 
 Note that in 42 those are all just expressions, and represents methods in the named class.
 Sometime is convenient to reuse this kind of syntax to get better syntactic support for certain operations:
@@ -286,7 +304,7 @@ Wlink(S, To understand it better: a guide to the S class)
 
 WTitle(Libraries)
 42 stands for the primate of libraries, so let see some libraries in action.
-We have already see how to chose a towel, and many classes that are likly to be present in such towel, like
+We have already see how to chose a towel, and many classes that are likely to be present in such towel, like
 Int and Data.
 Let see now how to load a library from its url:
 OCode
