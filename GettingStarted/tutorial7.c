01a527b46abe868282047ea864993d93976cd718
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2016-05-13 11:32:02 +1200

    Metaprograming

--- GettingStarted/tutorial7.c
+++ GettingStarted/tutorial7.c
@@ -38,11 +38,16 @@ MyGui:Gui(
   x:600Gui.Pixel//seek for index.html in your basePath
   y:400Gui.Pixel
   )<<{
-  method Void event_quit(Gui gui, S msg)
+  mut method
+  Void event_quit(mut Gui gui, S msg)
     gui.close()
-  method Void event_sayHi(Gui gui, S msg)
+
+  mut method
+  Void event_sayHi(mut Gui gui, S msg)
     Gui.alert(S"hi")
-  method Void event_say(Gui gui, S msg) (
+
+  mut method
+  Void event_say(mut Gui gui, S msg) (
     //msg is going to be a multiline string,
     //first line equal to "say", other lines give more info
     Strings ss=msg.split(S.nl())
@@ -75,13 +80,39 @@ To make something happen in the gui, you can use
 Wcode(gui.executeJs(cmd)).
 For example
 OBCode
-method event_writeOnTextArea(Gui gui, S msg) (
+mut method
+Void event_writeOnTextArea(mut Gui gui, S msg) (
   Strings ss=msg.split(S.nl())
   js1=Gui.JavaScript"document.getElementById('myTextAreaId').value = 'Hi! event happened';"
   //or, to use information in the message
   js2=Gui.JavaScript"document.getElementById('"[ss.val(1\)]"').value = '"[ss.val(2\)]"';"
   gui.executeJS(js2)
   )
+CCode
+
+Gui provide help to visualize datastructures of various kind, 
+for example vectors of Data classes can be shown as tables in the following way:
+OBCode
+Person:Data<<{Size id, Name name, Name surname, Year age}
+Persons:Collections.vector(of:Person)
+ShowPersons:Gui.widget(table:Persons)
+InputPerson:Gui.input(dialogForm:Person)//rely on JQuery UI
+MyGui:Gui(/*..*/)<<{ mut Persons persons //field
+  /*..*/
+  mut method
+  Void eventLoad(mut Gui gui, S msg) ( //no underscore for system events
+    gui.add(ShowPersons(this.#persons(), into:Gui.Id"divLeft"))
+    //the element with that id will contain the table
+    )
+  mut method
+  Void event_addPerson(mut Gui gui,S msg) (
+    Person p=InputPerson(gui,title:S"New Person details")
+    catch exception InputPerson.Cancelled void
+    this.persons().add(left:p)
+    gui.refresh()
+    )
+  }
+Main:MyGui(persons:Persons[])
 CCode
 
 WTitle((2/5) Files)
@@ -122,18 +153,153 @@ In general, all the system interaction that happens over the same system object 
 chronologically sorted with respect to each other, but there is no guarantee 
 of ordering between different system objects.
 
+WTitle((3/5) Db)
+In AdamTowel, databases can be access in two ways:
+Raw access (similar to what is supported by
+Wcode(DBC) or Wcode(JDBC) libraries) 
+and Structured access.
+OBCode
+{reuse L42.is/AdamTowel
+Db: Load<<{reuse L42.is/Db}//Db can do Raw access
+UnivDb:Db.importStructure(Db.ConnectionS"...")
+QueryCountry:UnivDb.Query"select * from students where country=@country"
+Main:{
+  connection=UnivDb.connect()
+  UnivDb.Student.Table ss=QueryCountry(connection, country:S"Italy")
+  /*..*/
+  }
+}
+CCode
+Here
+we use Wcode(Db) to 
+import the structure of the database. This means that 
+Wcode(UnivDb) will contain a class for each table-row of the database,
+every class will have a nested type representing 
+multiple rows (that is, a table).
+Wcode(UnivDb.Query) allows prepared queryes.
+In the case in the example, we can use
+Wcode(`QueryCountry(that,country)')
+to get the italian students.
+Note how Wcode(country) is a parameter of the method: in this way the  query users are informed of the query expectations.
+Our query used Wcode(*), so the type Wcode(UnivDb.Student.Table)
+could be reused.
+In other cases a new type would be generated.
+
+WTitle((4/5) example Gui and Db together)
+
+
+
+WTitle((5/5) Libraries, summary)
 
 WBigTitle(Refactor and Extend)
 
-WTitle(Refactor)
+WTitle((1/5)Refactor)
 Wcode(Refactor) is a class supporting modification of
 library literals.
-For example, you may want to 
+For example, you may want to rename the method Wcode(importStructure(that)) in just Wcode(import(that)).
+You can do the following:
+OBCode
+{reuse L42.is/AdamTowel
+Db:Refactor.RenameMethod(Selector"importStructure(that)" 
+  to:Selector"import(that)")<<Load<<{reuse L42.is/Db}
+UnivDb:Db.import(Db.ConnectionS"...")
+/*..*/
+}
+CCode
+The type Wcode(Selector) represent method selectors;
+in the same way the type Wcode(Path) represent 
+paths, as in Wcode(Path"Refactor.RenameMethod") or
+Wcode(Path"This").
 
-WTitle(Extend)
+There are a lot of refactoring operations nested under Wcode(Refactor):
+<ul><li>
+Wcode(RenameSelector)
+and 
+Wcode(RenamePath)
+rename methods either at top level (as we just did) or
+in an arbitrary nested library;
+or rename paths in other paths
+</li><li>
+
+Wcode(Redirect)
+allowing to remove a nested library and redirect all its references to
+an external one. This allows to emulate generics, as we will see later.
+</li><li>
+Wcode(UpdateDocumentationSelector)
+and Wcode(UpdateDocumentationPath)
+to add to/alter/delete the documentation of methods/paths.
+</li><li>
+Wcode(MakeAbstractSelector)
+and Wcode(MakeAbstractPath)
+to remove all the implementation out of a method or path,
+leaving only the public scheleton
+</li><li>
+Wcode(HideSelector)
+andWcode(HidePath)
+To mark methods or paths as private.
+We have not seen details on private members, the main idea is that
+they are renamed in invisible names that you can never guess, and automatically renamed on need by refactoring operations.
+</li></ul>
+WP
+In addition to all those nested classes,
+Wcode(Refactor) offers Wcode(`Refactor.compose(left,right)')
+allowing a simmetric sum of two library literals.
+The main idea is that members with the same name are recoursivelly summed
+
+WTitle((2/5)Traits and Metaprogramming)
+
+Metaprogramming is the most important feature of 42,
+all the decorators that you have seen up to now are implemented with metaprogramming, that in 42 offers a good balance of freedom and
+safety.
+The main idea of 42 metaprogramming is that only library literals can
+be manipulated, and that once a library literal has a name, it can not be directly metaprogrammed any more.
+Thus, we call traits methods that return reusable (unnamed) code.
+For example
+OBCode
+ReusableCode:{
+  Library traitEnsureTransaction() {
+    class method //without name: can be called as a funtor
+    Void(mut Db.Connection connection)
+    exception Db.Query.Failure //no body: abstract method
+
+    class method
+    Void doAction(mut Db.Connection that)
+    exception Db.Query.Failure (
+      that.openTransaction()
+      This(connection:that)//here we call us as a funtor
+      catch exception Db.Query.Failure x (
+        that.rollback()
+        exception x
+        )
+      that.commitTransaction()
+      )
+    }
+  }
+
+MyAction:Refactor.compose(
+  left:ReusableCode.traitEnsureTransaction()
+  right:{ //and now the missing implementation
+    Void(mut Db.Connection connection)
+    exception Db.Query.Failure {
+      /*..my operation..*/
+      }
+    })
+
+Main:{
+  /*..*/
+  MyAction(conn)//do the operation in a transaction
+  /*..*/
+  }
+CCode
 
 
 
+WTitle((3/5)Extend)
+
+
+WTitle((4/5)Exercise: extending vector)
+
+WTitle((5/5)Metaprogramming summary)
  <!--
  
  
