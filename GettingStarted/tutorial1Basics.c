51e794a2452f415336a101147c4b4bf4192502ca
MarcoServetto <marco.servetto@ecs.vuw.ac.nz>
2016-05-25 10:11:42 +1200

    Last Re read1/recover some text loss in a conflict 12/05

--- GettingStarted/tutorial1Basics.c
+++ GettingStarted/tutorial1Basics.c
@@ -1,8 +1,8 @@
 WBigTitle(Target audience)
 This tutorial is design for expert programmers, already knowledgeable in 
 at least two or three languages between Java, C#, C++ and Python.
-This tutorial lay basic knowledges for programming in 42 using AdamsTowel, but
-do not explore the foundational theories behind 42,
+This tutorial teach basic knowledge for programming in 42 using AdamsTowel, but
+does not explore the foundational theory behind 42,
 or the mathematical reasons that makes 42 correct.
 WBR
 AdamsTowel is in continue evolution; this document does not represent the current state
@@ -11,6 +11,7 @@ WBR
 This document is work in progress,
 and chapters 10 and 11 are not as well thought out as the rest of the document,
 and other chapters may still be inserted.
+The syntax highlighter is still in progress.
 
 WBigTitle(Basics)
 WTitle((1/5)Simple hello world program)
@@ -28,7 +29,7 @@ CCode
 When we write Wcode(reuse L42.is/AdamsTowel) we are asking 42 to
 reuse the code of the library found in the internet address 
 Wcode(L42.is/AdamsTowel).
-AdamsTowel is our Towel, that is the set of classes and interfaces that we wish to start from (Wlink(towel,see more on towels)).
+AdamsTowel is our Towel, that is the set of classes and interfaces that we wish to start from (Wlink(towel,Deploy code)).
 Wcode(L42.is) is the main website of 42, where most commonly used libraries are hosted. To reuse code you 
 need an internet connection; but this also means that you will never have to manually import any code.
 Required code will be transitively downloaded and cached on your machine, so you need not to be aware of the existence of this mechanism.
@@ -71,8 +72,8 @@ Main: {
   }
 }
 CCode
-Here we declare a class to host our Wcode(hello(name)) method.
-We write Wcode(class method) to declare a method that can be called on the class object, as in Wcode(MyCode.hello(name: S"world")).
+Here we declare a class to host our Wcode(hello(nickName)) method.
+We write Wcode(class method) to declare a method that can be called on the class object, as in Wcode(MyCode.hello(nickName: S"Marvin")).
 This is roughly equivalent to a static method in languages like Java or C++ , or class methods in Python.
 
 WP
@@ -81,7 +82,7 @@ We believe this increases readability.
 WP
 You may also notice how there are two different usages for curly brackets:  if there is at least one Wcode(return) keyword then the expression is a block of statements,
 otherwise the expression is a library literal, witch can contains methods and nested libraries.
-A Wemph(nested library)
+A WEmph(nested library)
 is denoted by an upper-case name, and can be obtained by a library literal or an expression producing a library literal.
 A library literal can be a class (default case) or an interface (starts with the Wcode(interface) keyword).
 A nested library in 42 is similar to a static inner class in Java, or a nested class in C++. It is just a convenient way to organize the various components of our program into a tree shape.
@@ -102,28 +103,33 @@ Point: Data <>< {
     this.with(y: y + this.y())
   }
 CCode
-Here you can see we declare a Wcode(Point) class with Wcode(x) and Wcode(y) Wcode(Num) (unlimited precision rational number) coordinates.
+Here you can see we declare a Wcode(Point) class with coordinates Wcode(x) and Wcode(y) of type Wcode(Num),
+unlimited precision rational number.
+
+
 Wcode(Data) is a decorator. Decorators are classes/objects that offer an operator Wcode(<><), called the babel fish operator,
 whose goal is to translate a library into a "better" library.
-In this case, Wcode(Data) is translating the class Wcode(`{Num x, Num y}') into a much longer class, with
-a factory method taking in input the fields and initializing them, but also containing
+In this case, Wcode(Data) is translating the class Wcode(`{Num x, Num y}')
+ into a much longer class, with
+a factory method taking in input the fields and initializing them; but also containing
  boring but useful definitions for
 equality, inequality, conversions from and to human readable strings, XML and binary representations for (de)serialization.
 WP
-Finally, we declare methods to add to a single coordinate.
-For very short methods we can omit the curly brackets and return.
-Indeed, method bodies are just expressions, and the curly brackets turn statements into expressions. 
+Finally, we declare a methods to add to each of the coordinates.
+For very short methods we can omit the curly brackets and Wcode(return).
+Indeed, method bodies are just expressions, and the curly brackets turna block of statements into one expression. 
 
-In the method Wcode(add(x)) we show a constructor call and getters.
-In the method Wcode(add(y)) we show an improved version, using the Wcode(with) method, another gift of Data, that allows us to easily create a clone with a
-single field updated.
+In the method Wcode(add(x)) we show a how to create a new Wcde(Point) instanceand how to call getters.
+In the method Wcode(add(y)) we show an improved version, using the Wcode(with) method, another gift of Data, that allows us to easily create a clone with one or more fields updated.
 We can declare two methods, Wcode(add(x)) and Wcode(add(y)) with the same name, if parameter names are different.
 WP
 Note how we always use getters and we never access fields directly.
-In many other languages we can use write Wcode(a.fieldName) and Wcode(a.fieldName: newValue). Such syntax does not exists in 42. The same goes for object instantiation; in many languages there is a special Wcode(new ClassName(..)) dedicated syntax, while in 42 it is just a method call.
+In many other languages we can use write Wcode(a.fieldName) and Wcode(a.fieldName=newValue). Such syntax does not exists in 42. The same goes for object instantiation; in many languages there is a special Wcode(new ClassName(..)) dedicated syntax, while in 42 it is just a method call.
 WP
 Also, similarly to what happens in Python, we need to use Wcode(this.methodName()) to call methods when the receiver is Wcode(this).
-While this makes some code more verbose, it saves us from the burden of  method hiding.   
+While this makes some code more verbose, it saves us from the burden of
+method scope-hiding.  ; that is,
+we do not need to worry about scoping and nesting for method resolution.
 
 
 
@@ -152,13 +158,13 @@ CCode
 Vectors can be initialized with the Wcode([_;_;_])
 syntax or with the Wcode([with _ ( _ use[ _ ]_ )]) syntax.
 We will discuss all the variation of Wcode(with) later.
-Note how to express number literal we need to specify their class.
+Note that to express number literals we need to specify their class.
 
 WTitle(`(5/5)Basis summary')
 
 <ul><li>
 At the start of your program, import a towel using 
-"Wcode(reuse _)", as in Wcode(reuse L42.is/AdamsTowel).
+Wcode(reuse _), as in Wcode(reuse L42.is/AdamsTowel).
 </li><li>
 To declare a simple class exposing its state and 
 some methods working with those, use Wcode(Data), as in
@@ -176,7 +182,7 @@ Wcode(Points: Collections.vector(of: Point))
 
 
 WTitle(`Object creation summary')
-42 supports many different syntactic forms that are convenient to create objects: 
+42 supports many different syntactic forms that are convenient for creating objects: 
 <ul><li>
 12Num:  from a numeric representation
 </li><li>
@@ -184,9 +190,9 @@ S"foo":  from a string representation
 </li><li>
 Point(x: _,y: _):  from the parameter values
 </li><li>
-Points[_;_;_]:  from a variable length sequence of values; for example Wcode(Points) can be a list of Wcode(Point). We can instantiate Wcode(Points) by using the square brackets as shown later. 
+Points[_;_;_]:  from a variable length sequence of values.
 </li></ul>
 
 Note that in 42 those are all just expressions, and represent methods in the named class.
-Sometimes it is convenient to reuse this kind of syntax to get better syntactic support for certain operations; for example, the string class uses square brackets to support string formatting.
-Wlink(S, To understand it better:  a guide to the S class)
+Sometimes it is convenient to reuse this kind of syntax to get better syntactic support for certain operations; for example, as we will see later, the string class uses square brackets to support string formatting.
+Wlink(S,`Collections and Sequences')
