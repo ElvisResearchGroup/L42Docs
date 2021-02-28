90da6011d3dae254afadd869d8c51c090b1ad483
Marco.Servetto <Marco.Servetto@gmail.com>
2021-02-28 21:25:24 +1300

    .

--- GettingStarted/tutorial11MetaprogrammingIntro.c
+++ GettingStarted/tutorial11MetaprogrammingIntro.c
@@ -387,7 +387,239 @@ When reusing code, we have to be mindful of missing types and missing methods. S
 
 WBigTitle(`Traits and rename')
 
-WTitle((1/5)Programmatic refactoring)
+WTitle((1/5)An introduction to Programmatic Refactoring)
+
+Traits allows to programmatically rename methods and nested classes to other names.
+Consider the following example code, defining trait Wcode(Code1), containing many nested classes:
+the interface Wcode(A), the class Wcode(B) implementing Wcode(A),
+the class Wcode(C), that is similar to Wcode(B) but does not impliements Wcode(A)
+and finally the class Wcode(D) that uses both Wcode(B) and Wcode(C):
+
+OBCode
+Code1 = Trait:{
+  A = {interface,  method S m()}
+  B = {[A],        method S m()=S"Hi"                    class method This()}
+  C = {            method S m()=S" world"                class method This()}
+  D = {            method S callBoth()=B().m()++C().m()  class method This()}
+  }
+Concrete1 = Class:Code1
+Main1 = Debug(Concrete1.D().callBoth())
+CCode
+We can extract the code into class Wcode(Concrete1), and then use it to print 
+Wcode("Hi World").
+We show below how we can use the operator Wcode([_]) to programmatically rename elements of Wcode(Code1) while preserving the semantic:
+OBCode
+Concrete2 = Class:Code1['A.m()=>'A.k()]
+Concrete2 = {//eqivalent to just writing the following directly:
+  A = {interface,  method S k()}
+  B = {[A],        method S k()=S"Hi"                    class method This()}
+  C = {            method   m()=S" world"                class method This()}
+  D = {            method S callBoth()=B().k()++C().m()  class method This()}
+  }
+Main2 = Debug(Concrete2.D().callBoth()) //still prints "Hi World"
+CCode
+As you can see above, the mapping Wcode('A.m()=>'A.k()) renamed all
+the occurences of Wcode(A.m()) into Wcode(A.k()). This includs all the
+declarations refining such method and all the usages of such method, either from the
+static type Wcode(A) or from any nested class implementing Wcode(A).
+On the other side, you can see how Wcode(C().m()) was not renamed; the rename is type driven.
+We can also rename multiple names at once; in this way we can even swap names:
+
+OBCode
+Concrete2 = Class:Code1[
+  'A.m()=>'A.k();
+  'B.#apply() =>'B.of();
+  'C=>'D;
+  'D=>'C;
+  ]
+Concrete3 = {//eqivalent to just writing the following directly:
+  A = {interface,  method S k()}
+  B = {[A],        method S k()=S"Hi"                    class method This of()}
+  C = {            method S callBoth()=B.of().k()++D().m()  class method This()}
+  D = {            method   m()=S" world"                class method This()}
+  }
+Main3 = Debug(Concrete3.C().callBoth()) //still prints "Hi World"
+CCode
+Note how the call Wcode(B()) is now replaced with Wcode(B.of()).
+Wcode(#apply()) is the extended name for the method with the empty name.
+Also binary operators methods can be renamed by using their extended name;
+The complete list of extended names for binary operators is discusssed later, but you can also just use the overview feature to see them in any compiled class.
+The Wcode(') sign is a syntactic sugar similar to the Wcode(\);
+indeed Wcode('Foo.bar(x)) is equivalent to
+Wcode(\"Foo.bar(x)"); where the text after the Wcode(') have strict syntactic restrictions, requiring it to be either a valid path (as Wcode(Beer.Foo) or Wcode(This), for example), a valid method selector, or 
+a path followed by a method selector.
+A method selector can also be followed by an argument name, as in Wcode('Foo.bar(x).x)
+In 42 programmatic refactoring and other tasks requiring to express paths and method selectors are very common, and writing Wcode(myTrait['A=>'B]) is so much more convenient that writing Wcode(myTrait[Name"A"=>Name"B"]).
+
+WTitle((2/5) Programmatic Refactoring: all kinds of operations)
+WTitle(Single)
+Programmatic refactoring of nested classes is transitive on default.
+All the nested classes are going to be renamed together with the renamed root.
+The code below shows how to specify a single rename instead:
+OBCode
+MultiLevel = Trait:{
+  A = {
+    class method S hi() = S"hi"
+    B = {class method S world() = S"world"}
+    }
+  C = {
+    class method Void print() = Debug(A.hi()++D.space()++A.B.world())
+    D = { class method S space() = S" " }
+    }
+  }
+MultiConcrete = Class:MultiLevel[ single='C=>'K; 'A=>'D.H ]
+MultiConcrete = {//eqivalent to just writing the following directly:
+  D = {
+    H = {
+      class method S hi() = S"hi"
+      B = {class method S world() = S"world"}
+      }
+    }
+  C = {
+    D = { class method S space() = S" " }
+    }
+  K = {
+    class method Void print() = Debug(D.H.hi()++C.D.space()++D.H.B.world())
+    }
+  }
+Main4 = MultiConcrete.K.print() //still prints "hi world"
+CCode
+As you can see, we did a single rename Wcode('C=>'D) and a transitive rename Wcode('A=>'D.H).
+Since there where nested classes inside of Wcode(C), the single rename have left a shell of
+Wcode(C) in place so that the nested Wcode(C.D) could stay in position.
+WP
+
+We can also rename Wcode('This).
+For example, with the code below we can make the original top level into a nested class, and the nested class Wcode(NewTop) into the top level:
+OBCode
+Res=Class:Trait({
+  method S originalTop()=S"originalTop"
+  NewTop ={method S newTop()=S"newTop"}
+  })[single='This=>'OriginalTop;single='NewTop=>'This]
+CCode
+WP
+On the other side, self rename, for example Wcode('C=>'C) is usually an error, and thus it will raise an exception. However, we can silent such error and turn self rename into a no-op by using
+Wcode(myCode[ignoreSelfRename='C=>'C]) or Wcode(myCode[ignoreSelfRenameSingle='C=>'C]).
+
+WTitle(Hide)
+The code below shows how to  hide a method or a class:
+OBCode
+MultiConcrete2 = Class:MultiLevel[ hide='A; hide='D.space() ]
+MultiConcrete2 = {//eqivalent to just writing the following directly:
+  A::3 = {
+    class method S hi::4() = S"hi"
+    B::5 = {class method S world::6() = S"world"}
+    }
+  C = {
+    class method Void print() = Debug(A::3.hi::4()++D.space::7()++A:3.B::5.world::6())
+    D = { class method S space::7() = S" " }
+    }
+  }
+Main5 = MultiConcrete2.C.print() //still prints "hi world"
+CCode
+
+Private members in 42 are obtained by relying on unique unguessable numbers:
+Those names does not show up in the outline and can not be invoked by user code; moreover those numbers are automatically renamed to avoid clashing during code composition; overall, those numbers are completelly invisible for the user of the code.
+While it is possible to manually use unique numbers, it is so much more convenient to write open code and then seal it later.
+
+WTitle(Clear)
+Hidden code is still part of the result, but it is no more accessible.
+Simmetrically, cleared code is not part of the result, but its entry point is still accessible, but abstract; clearing code allows to override already defined behaviour, as shown below:
+
+OBCode
+MultiConcrete3 = Class:MultiLevel[ clear='A ]:{
+  A = {
+    class method S hi() = S"hello"
+    B = {class method S world() = S"42"}
+    }
+  }
+Main6 = MultiConcrete3.C.print() //now prints "hello 42"
+CCode
+
+Of course, Wcode(clearSingle='A) would clear only the nested class Wcode(A) and not also Wcode(A.B).
+
+
+WTitle(Soft rename)
+Clearing code allows to override code by removing code.
+This is different with respect to what happens with overriding in most languages, where the former code still exists and can be invoked, for example with Wcode(super).
+
+In 42 we can easily emulate super by using Wcode(->) instead of Wcode(clear); the code below shows how Wcode(->) can be used on both methods and nested classes:
+OBCode
+MultiConcrete4 = Class:MultiLevel[ 'A->'SuperA; 'C.D.space()->'C.D.superSpace() ]:{
+  SuperA = { class method S hi() B={class method S world()}}
+  A = {
+    class method S hi() = S"[%SuperA.hi()]"
+    B = {class method S world() = S"[%SuperA.B.world()]"}
+    }
+  C={
+    D={
+      class method S superSpace()
+      class method S space()=S"[%this.superSpace()]"
+      }
+    }
+  }
+Main7 = MultiConcrete4.C.print() //now prints "[hi][ ][world]"
+CCode
+
+Note how in this case we explicitly declare Wcode(SuperA.hi()), Wcode(SuperA.B.world()) and
+Wcode(C.D.superSpace()) in the composed code, even if they are already present in the 
+result of 
+Wcode(MultiLevel[ 'A->'SuperA; 'C.D.space()->'C.D.superSpace() ]).
+
+We will soon show a way to avoid redeclaring them, but our experience programming in 42 suggests that when only a few methods are involved, the code is often more clear and easier to understand by redeclaring them.
+
+
+WTitle(Redirect)
+Finally, programmatic refactory allows us to rename a nested class into an externally declared class.
+This also provideds a simple encoding for generics.
+Consider the following code:
+OBCode
+BoxTrait = Trait:Data:{
+  T = {}//declaring a nested class called 'T'
+  T that //a field of type T called 'that'
+  }
+NumBox = Class:BoxTrait['T=>Num]
+Main = (
+  myBox = NumBox(3Num)
+  Debug(myBox)
+  Num n = myBox.that()
+  Debug(n)
+  )
+CCode
+Note how we wrote Wcode(BoxTrait['T=>Num]) and not Wcode(BoxTrait['T=>'Num]):
+We are using Wcode(Num) as the numeric class defined outside, we are not referring to a 
+class nested inside Wcode(BoxTrait) and called Wcode(Num).
+Generics classes are straightforward to implement with redirect, and indeed
+Wcode(Collection) uses the redirect operator internally.
+
+
+WTitle((3/5) Different ways to supply missing dependencies)
+
+
+-talk about sum-rename
+
+WTitle((4/5) Introspection and Info)
+-talk about info
+
+
+We can use two level of renames to avoid having to redeclare them:
+
+MultiConcrete5 = Class:Trait({
+  $ = Class.Relax:MultiLevel[ 'A->'SuperA; 'C.D.space()->'C.D.superSpace() ]
+  A = {
+    class method S hi() = S"[%$.SuperA.hi()]"
+    B = {class method S world() = S"[%$.SuperA.B.world()]"}
+    }
+  C={ D={ class method S space()=S"[%$.C.D.superSpace()]" } }
+  })['$=>'This]
+  
+Main8 = MultiConcrete5.C.print() //now prints "[hi][ ][world]"
+  
+Concrete3 = Class:Code1['C.m()->'C.superM()]
+  : {C={method S superM() method S m()=this.superM()++S"!" } }
+Main3 = (
+  Debug(Concrete3.D().callBoth())
+  )
 
 
 Operator Wcode(:) merges multiple units of code together; but sometime code needs some kind of adaptation before it can be merged
